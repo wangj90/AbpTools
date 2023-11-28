ab145f5ae17c34b57a4cfba7555c549fb6670d51
wangjun <wang_j90@126.com>
2023-11-28 15:17:58 +0800

    初始化FluentUI的组件

--- AbpTools/Program.cs
+++ AbpTools/Program.cs
@@ -1,5 +1,6 @@
 using AbpTools;
 using AbpTools.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 using Volo.Abp;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddFluentUIComponents();
 
 await builder.Services.AddApplicationAsync<AbpToolsModule>();
 
